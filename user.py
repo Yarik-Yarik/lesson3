1040826c395d00f0afa00cd6e2b770d798dc06e3
Ярик-Ярик <btsov2012@mail.ru>
2024-10-13 12:52:10 +0300

    lesson_3

---
+++ user.py
@@ -0,0 +1,20 @@
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def print_first_name(self):
+        print(f"Имя: {self.first_name}")
+
+    def print_last_name(self):
+        print(f"Фамилия: {self.last_name}")
+
+    def print_full_name(self):
+        print(f"Полное имя: {self.first_name} {self.last_name}")
+
+
+my_user = User("Иван", "Иванов")
+
+my_user.print_first_name()
+my_user.print_last_name()
+my_user.print_full_name()
